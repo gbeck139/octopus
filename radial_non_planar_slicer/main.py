63ceb40fefdd58db84677ed999f6371f76d9b690
Grant Beck <gbeck139@gmail.com>
2025-10-17 16:44:05 -0600

    Tested with motorcycle model

--- radial_non_planar_slicer/main.py
+++ radial_non_planar_slicer/main.py
@@ -7,7 +7,7 @@ from scipy.spatial.transform import Rotation as R
 import deform
 
 
-MODEL_NAME = 'propeller'
+MODEL_NAME = 'motos_para_a_eternidade'
 
 mesh = deform.load_mesh(MODEL_NAME)
 deformed_mesh, ROTATION, offsets_applied = deform.deform_mesh(mesh)
