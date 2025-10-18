dc69590db1fa310eb328a3574cf9aab752c6d46f
Marina Scott <ironduckmei@gmail.com>
2025-10-17 22:08:51 -0600

    Updated .gitignore to stop tracking Qt build folder

--- gui/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_gui_FILE C:/Users/canca/Documents/QTProjects/SlicerApp/gui/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/gui.exe)
-set(__QT_DEPLOY_TARGET_gui_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_gui_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
